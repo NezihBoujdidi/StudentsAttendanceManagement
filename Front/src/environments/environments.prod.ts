34f9eda95fc7a557a56fe844bf3fba16b0a0de10
nezih <nezihboujdidi02@gmail.com>
2024-11-26 10:01:32 +0100

    added environment prod settings

---
+++ Front/src/environments/environments.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://accurate-insight-production.up.railway.app' 
+  };
+  
---
+++ Front/src/environments/environments.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:8081' // Replace with your local backend URL
+  };
+  
